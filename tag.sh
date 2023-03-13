24cd5ac07cfa69e47b74dbde0902393756c3da2f
Wojciech Jedynak <wjedynak@gmail.com>
2023-03-13 15:44:23 +0900

    Fix

--- tag.sh
+++ tag.sh
@@ -4,4 +4,4 @@ LATEST_TAG=$(git --no-pager tag --points-at HEAD)
 echo ${LATEST_TAG}
 
 # replace contents
-sed "s/{{TODO}}/${LATEST_TAG}/" index.html.template
+sed "s/{{TODO}}/${LATEST_TAG}/" index.html.template >index.html
