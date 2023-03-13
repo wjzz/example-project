85119827d6a97dc1ed314032c605b35ec6517fc8
Wojciech Jedynak <wjedynak@gmail.com>
2023-03-13 15:40:13 +0900

    Reorganize

---
+++ Makefile
@@ -0,0 +1,6 @@
+.PHONY: build
+
+build:
+	@echo "Started Building the page"
+	./tag.sh
+	@echo "Finished Building the page"
---
+++ tag.sh
@@ -0,0 +1,7 @@
+set -e
+
+LATEST_TAG=$(git --no-pager tag --points-at HEAD)
+echo ${LATEST_TAG}
+
+# replace contents
+sed "s/{{TODO}}/${LATEST_TAG}/" index.html.template
